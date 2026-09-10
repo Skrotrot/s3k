fc9a920c180b15b0725363479fee622151fae488
Jack <jack@example.com>
2026-09-10 10:08:21 +0200

    Implemented the makro on the vulnerable app.
    The makro successfully detected the change in the return address before
    the process returned.
    
    Now some other function has to be ran in the makro to enforce the

---
+++ projects/makro-on-vulnerable-app/app0/main.c
@@ -0,0 +1,56 @@
+#include "altc/altio.h"
+#include "altc/string.h"
+#include "s3k/s3k.h"
+
+#include "../../tutorial-commons/utils.h"
+
+static int _call_depth = 0;
+
+#define CFI_ENTER() \
+    void *__cfi_ra; \
+    __asm__ volatile ("mv %0, ra" : "=r"(__cfi_ra)); \
+    alt_printf("Enter %d   0x%x -\n", ++_call_depth, __cfi_ra)
+
+#define CFI_RETURN(...) \
+    do { \
+        void *__cfi_ra_now; \
+        __asm__ volatile ("ld %0, 40(sp)" : "=r"(__cfi_ra_now)); \
+        alt_printf("Exit  %d              -> 0x%x\n", \
+                   _call_depth--, __cfi_ra_now); \
+        return __VA_ARGS__; \
+    } while (0)
+
+void win()
+{
+    alt_printf("Attack Works\n");
+    do {} while(1);
+}
+
+static char source[24];
+
+void vulnerable(void)
+{
+    CFI_ENTER();
+    char buffer[2];
+
+    alt_printf("Inside\n");
+
+    memset(source, 'A', 32); //Offset to RA
+
+    *(uint64_t *)&source[32] = (uint64_t)win; // Overwrite RA with win()
+
+    memcpy(buffer, source, sizeof(source) + 64);
+    CFI_RETURN();
+}
+
+int main(void)
+{
+    // Setup UART access
+    setup_uart();
+
+    alt_printf("Before\n");
+
+    vulnerable();
+
+    alt_printf("Successful exit\n");
+}
